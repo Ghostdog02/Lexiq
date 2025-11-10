648881e85d6819ae656328d1af1aa485c49100e3
Ghostdog02 <alex.vesely07@gmail.com>
2025-11-10 14:48:32 +0200

    Ignoring files starting with env in .gitignore

---
+++ backend/Models/GoogleRequest.cs
@@ -0,0 +1,14 @@
+namespace Lexiq.Api.Models
+{
+    /// <summary>
+    /// Google login request
+    /// </summary>
+    public class GoogleLoginRequest
+    {
+        /// <summary>
+        /// JWT token from Google (with or without 'Bearer ' prefix)
+        /// </summary>
+        /// <example>Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI...</example>
+        public required string JwtToken { get; set; }
+    }
+}
