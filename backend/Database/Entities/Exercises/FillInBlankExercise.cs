cd33efb1050e1a68321b3c5f3d76c3b93c1b8baf
Ghostdog02 <alex.vesely07@gmail.com>
2026-04-29 14:24:04 +0300

    Update FillInBlank and Listening exercise implementations

--- backend/Database/Entities/Exercises/FillInBlankExercise.cs
+++ backend/Database/Entities/Exercises/FillInBlankExercise.cs
@@ -5,18 +5,8 @@ namespace Backend.Database.Entities.Exercises;
 public class FillInBlankExercise : Exercise
 {
     [Required]
+    [MaxLength(5000)]
     public required string Text { get; set; }
 
-    [Required]
-    [MaxLength(500)]
-    public required string CorrectAnswer { get; set; }
-
-    [MaxLength(1000)]
-    public string? AcceptedAnswers { get; set; }
-
-    [Required]
-    public required bool CaseSensitive { get; set; } = false;
-
-    [Required]
-    public required bool TrimWhitespace { get; set; } = true;
+    public List<ExerciseOption> Options { get; set; } = [];
 }
--- backend/Database/Entities/Exercises/ListeningExercise.cs
+++ backend/Database/Entities/Exercises/ListeningExercise.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace Backend.Database.Entities.Exercises;
 
 public class ListeningExercise : Exercise
@@ -8,16 +7,8 @@ public class ListeningExercise : Exercise
     [MaxLength(500)]
     public required string AudioUrl { get; set; }
 
-    [Required]
-    [MaxLength(500)]
-    public required string CorrectAnswer { get; set; }
-
-    [MaxLength(1000)]
-    public string? AcceptedAnswers { get; set; }
-
-    [Required]
-    public bool CaseSensitive { get; set; } = false;
-
     [Range(1, 10)]
     public int MaxReplays { get; set; } = 3;
+
+    public List<ExerciseOption> Options { get; set; } = [];
 }
